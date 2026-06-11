93fa93f89590a459a66f83ed3d187866e99eb1a4
jyjmt2002 <jyjmt2002@gmail.com>
2026-06-11 13:09:24 +0900

    hero image rerere

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import {
   AnimatePresence 
 } from "motion/react";
 const logoImage = "https://193igusa.com/cdn/shop/files/nelogo_828ff3f3-8acf-431c-a30b-5c6b46acd0f8.png?v=1777452430&width=800";
-import heroImage from "./assets/images/uploaded_hero_image_1781072682088.png";
+const heroImage ="/images/hero.png";
 import { 
   Heart, 
   ShoppingBag, 
